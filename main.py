35c75ae02d18d177434e1b9dc679c0c3866f7e72
Dale E. Moore <DaleEMoore@gMail.Com>
2015-05-04 10:50:45 -0500

    in sync

--- main.py
+++ main.py
@@ -13,80 +13,85 @@ import sys
 import datetime
 import time
 
-# calculate one week ago for the date comparison
-now = datetime.datetime.now()
-lastWeek = now + datetime.timedelta(days=-7)
-#print('       Now:' + str(now))
-print("Remove files created last week or before:" + str(lastWeek))
-#date += datetime.timedelta(days=1)
+def main(parser2, argv2):
+    # calculate one week ago for the date comparison
+    now = datetime.datetime.now()
+    lastWeek = now + datetime.timedelta(days=-7)
+    #print('       Now:' + str(now))
+    print("Remove files created last week or before:" + str(lastWeek))
+    #date += datetime.timedelta(days=1)
 
 
-# TODO; figure out how to tell this program to delete everything except a list of keeps:
-#    Keep Everyday for the last month.
-#    Keep Every Friday for the last quarter.
-#    Keep The first day of every month for the last 2 years.
+    args = parser2.parse_args()
+    rmDsp = ""
+    if args.rm:
+        print('Remove filtered files')
+        rmDsp = "Remove"
+    else:
+        print('Display filtered files, do NOT remove')
+        rmDsp = "Do NOT Remove"
+    #print ('parser:' + str(parser))
+    #print('args:' + str(args))
+    #print('vars(args):' + str(vars(args)))
+    #print(args.folder)
+    #TypeError: 'Namespace' object has no attribute '__getitem__'
+    #print(args['folder'])
+    #print(parser.parse_args(['-folder']))
 
+    #print(args.accumulate(args.integers))
 
-parser = argparse.ArgumentParser(description='Remove files and folders from a folder defined by a filter.')
-parser.add_argument('-folder', type=str, nargs='+', help='the folder to remove files and folders from.')
-parser.add_argument('-rm', action='store_true', default=False, help='remove the files, otherwise just display.')
-parser.add_argument('-keepfriday', action='store_true', default=False, help='keep files and folders created on Fridays.')
-#parser.add_argument('-b', action='store_true', default=False)
-#parser.add_argument('-rm', type=str, nargs='+', help='remove the files, otherwise just display')
+    if args.folder == None:
+        print("ERROR: No folders on command line!")
+        parser.print_help()
+        exit()
 
-args = parser.parse_args()
-rmDsp = ""
-if args.rm:
-    print('Remove filtered files')
-    rmDsp = "Remove"
-else:
-    print('Display filtered files, do NOT remove')
-    rmDsp = "Do NOT Remove"
-#print ('parser:' + str(parser))
-#print('args:' + str(args))
-#print('vars(args):' + str(vars(args)))
-#print(args.folder)
-#TypeError: 'Namespace' object has no attribute '__getitem__'
-#print(args['folder'])
-#print(parser.parse_args(['-folder']))
+    # iterate through folder(s)
+    for s1 in args.folder:
+        #print('Processing:' + s1)
+        for file in os.listdir(s1): # os.walk(s1) walks down the tree.
+        #for subdir, dirs, files in os.walk(s1): # os.walk(s1) walks down the tree.
+            fullFile = os.path.join(s1, file)
+            if not os.path.isdir(fullFile):
+                # get files older than 7 days.
+                (mode, ino, dev, nlink, uid, gid, size, atime, mtime, ctime) = os.stat(fullFile)
+                adate = datetime.datetime.fromtimestamp(atime)
+                mdate = datetime.datetime.fromtimestamp(mtime)
+                cdate = datetime.datetime.fromtimestamp(ctime)
+                if cdate <= lastWeek:
+                    print((rmDsp + " " + fullFile + " created: %s" % time.ctime(ctime)))
+                    if args.rm:
+                        print("Should remove " + fullFile)
+                        #os.remove(fullFile)
 
-#print(args.accumulate(args.integers))
+                    #print((" created: %s" % time.ctime(ctime)))
+                    #print(("accessed: %s" % time.ctime(atime)) + " " + str(adate) )
+                    #print(("modified: %s" % time.ctime(mtime)) + " " + str(mdate) )
+                    #print((" created: %s" % time.ctime(ctime)) + " " + str(cdate) )
+                    #print(fullFile)
+                    #print(os.path.join(subdir, file)) # us with os.walk()
 
-if args.folder == None:
-    print("ERROR: No folders on command line!")
-    parser.print_help()
-    exit()
+                    # delete selected file
+                    # if -rm on command line kill 'em otherwise just display.
+                    # os.remove(fullFile)
 
-# iterate through folder(s)
-for s1 in args.folder:
-    #print('Processing:' + s1)
-    for file in os.listdir(s1): # os.walk(s1) walks down the tree.
-    #for subdir, dirs, files in os.walk(s1): # os.walk(s1) walks down the tree.
-        fullFile = os.path.join(s1, file)
-        if not os.path.isdir(fullFile):
-            # get files older than 7 days.
-            (mode, ino, dev, nlink, uid, gid, size, atime, mtime, ctime) = os.stat(fullFile)
-            adate = datetime.datetime.fromtimestamp(atime)
-            mdate = datetime.datetime.fromtimestamp(mtime)
-            cdate = datetime.datetime.fromtimestamp(ctime)
-            if cdate <= lastWeek:
-                print((rmDsp + " " + fullFile + " created: %s" % time.ctime(ctime)))
-                if args.rm:
-                    print("Should remove " + fullFile)
-                    #os.remove(fullFile)
+        # TODO; set ctime for testing? or should this be in-code since setting ctime is sometimes not practical.
+        # this says it's impossible.
+        # http://stackoverflow.com/questions/4537291/setting-creation-or-change-timestamps
+        # Perhaps I can create a unit test that would do a good job of faking it out.
+    return True
 
-                #print((" created: %s" % time.ctime(ctime)))
-                #print(("accessed: %s" % time.ctime(atime)) + " " + str(adate) )
-                #print(("modified: %s" % time.ctime(mtime)) + " " + str(mdate) )
-                #print((" created: %s" % time.ctime(ctime)) + " " + str(cdate) )
-                #print(fullFile)
-                #print(os.path.join(subdir, file)) # us with os.walk()
+if __name__ == "__main__":
+    # TODO; figure out how to tell this program to delete everything except a list of keeps:
+    #    Keep Everyday for the last month.
+    #    Keep Every Friday for the last quarter.
+    #    Keep The first day of every month for the last 2 years.
 
-                # delete selected file
-                # if -rm on command line kill 'em otherwise just display.
-                # os.remove(fullFile)
+    parser = argparse.ArgumentParser(description='Remove files and folders from a folder defined by a filter.')
+    #print ("running:" + parser.prog)
+    parser.add_argument('-folder', type=str, nargs='+', help='the folder to remove files and folders from.')
+    parser.add_argument('-rm', action='store_true', default=False, help='remove the files, otherwise just display.')
+    parser.add_argument('-keepfriday', action='store_true', default=False, help='keep files and folders created on Fridays.')
+    #parser.add_argument('-b', action='store_true', default=False)
+    #parser.add_argument('-rm', type=str, nargs='+', help='remove the files, otherwise just display')
 
-    # TODO; set ctime for testing? or should this be in-code since setting ctime is sometimes not practical.
-    # this says it's impossible.
-    # http://stackoverflow.com/questions/4537291/setting-creation-or-change-timestamps
-    # Perhaps I can create a unit test that would do a good job of faking it out.
+    main(parser)
--- tests.py
+++ tests.py
@@ -1,29 +1,123 @@
 __author__ = 'DaleEMoore@gMail.Com'
+# From https://docs.python.org/2/library/unittest.html and others.
 
+import argparse
+import copy
+import os
+import shutil
+import sys
+import time
 import unittest2 as unittest
 
-# From https://docs.python.org/2/library/unittest.html
+theDir = "testFolder1"
+
+#def setUpModule():
+#    print ("setUp before Module")
+
+#def tearDownModule():
+#    print ("tearDown after Module")
+
+def createFolder(folderName, timeMod=time.time()):
+    print ("Create " + folderName + ".")
+    try:
+        os.makedirs(folderName)
+    except:
+        #print ("Folder " + folderName + " already exists.")
+        pass
+    os.utime(folderName, (timeMod,timeMod))
+
 class TestStringMethods(unittest.TestCase):
 
-  def test_zipper(self):
-      self.assertEqual('foo'.upper(), 'FOO')
+    @classmethod
+    def setUpClass(cls):
+        print ("setUp before Class")
 
-  def test_upper(self):
-      self.assertEqual('foo'.upper(), 'FOO')
+        # create folders and files for testing.
+        createFolder(theDir)
+        createFolder(theDir + os.sep + "d1", time.mktime(time.strptime('16.01.2014 00:00:00', '%d.%m.%Y %H:%M:%S'))) # 16 Jan 2014
+        createFolder(theDir + os.sep + "d2", time.mktime(time.strptime('01.05.2015 00:00:00', '%d.%m.%Y %H:%M:%S'))) # 15 May 2015 Friday
+        createFolder(theDir + os.sep + "d3", time.mktime(time.strptime('31.12.2014 00:00:00', '%d.%m.%Y %H:%M:%S'))) # 31 December 2014 EOY
 
-  def test_isupper(self):
-      self.assertTrue('FOO'.isupper())
-      self.assertFalse('Foo'.isupper())
+        #s1 = theDir + os.pathsep + "d1"
+        #time2 = time.mktime(time.strptime('16.01.2014 00:00:00', '%d.%m.%Y %H:%M:%S'))
+        # http://stackoverflow.com/questions/21156145/modify-file-create-access-write-timestamp-with-python-under-windows
 
-  def test_split(self):
-      s = 'hello world'
-      self.assertEqual(s.split(), ['hello', 'world'])
-      # check that s.split fails when the separator is not a string
-      with self.assertRaises(TypeError):
-          s.split(2)
+        # Wait for ENTER to continue doesn't run well in PyCharm test runner.
+        #try:
+        #    print ("Tap ENTER to continue:")
+        #    reply = input("Tap ENTER to continue:")
+        #except:
+        #    pass
 
-  def test_algebra(self):
-      self.assertEqual('foo'.upper(), 'FOO')
+
+    @classmethod
+    def tearDownClass(cls):
+        print ("tearDown after Class")
+
+        # delete folders and files from testing.
+        print ("Delete all files and folders under " + theDir + ".")
+        whatHappened = shutil.rmtree(theDir)
+        print ("whatHappened from Delete:" + str(whatHappened))
+
+    def test_rmFilter(self):
+        print ("\nTest running rmFilter against the folders that have been setup.")
+
+        # TODO; setup args like command line
+        # Like: http://stackoverflow.com/questions/24397258/can-not-run-nosetests-when-i-use-argparse-in-my-python-code
+        #parser = argparse.ArgumentParser()
+        #print ("running:" + parser.prog)
+        #parser.add_argument("echo")
+        #args = parser.parse_args()
+        #print ("args:" + args.echo)
+
+        parser = argparse.ArgumentParser(description='Remove files and folders from a folder defined by a filter.')
+        parser.add_argument('--folder', type=str, nargs='+', help='the folder to remove files and folders from.')
+        # --folder is acceptable reduced to -f.
+        parser.add_argument('-rm', action='store_true', default=False, help='remove the files, otherwise just display.')
+        parser.add_argument('--keepfriday', action='store_true', default=False, help='keep files and folders created on Fridays.')
+        # TODO; something like: args = "main.py -folder ."
+        # Like: http://stackoverflow.com/questions/24397258/can-not-run-nosetests-when-i-use-argparse-in-my-python-code
+        parser2 = copy.copy(parser)
+        argv2 = '--foo .'.split()
+        #argv2 = ['--folder','.']
+        #argv2 = copy.copy(sys.argv)
+        #args = parser2.parse_args()
+        # http://stackoverflow.com/questions/18668947/how-do-i-set-sys-argv-so-i-can-unit-test-it
+        import main
+        main.main(parser2, argv2)
+        # TODO; unit2 runs here because parser is processed twice and uses my default options for rmFilter
+        #usage: unit2 [-h] [-folder FOLDER [FOLDER ...]] [-rm] [-keepfriday]
+        #unit2: error: unrecognized arguments: --verbose
+        #ERROR
+
+
+
+    #def setUp(self):
+    #    print ("setUp before each test")
+
+    #def tearDown(self):
+    #    print ("tearDown after each test")
+
+    #def test_zipper(self):
+    #  self.assertEqual('foo'.upper(), 'FOO')
+
+    #def test_upper(self):
+    #  self.assertEqual('foo'.upper(), 'FOO')
+
+    #def test_isupper(self):
+    #  self.assertTrue('FOO'.isupper())
+    #  self.assertFalse('Foo'.isupper())
+
+    #def test_split(self):
+    #  s = 'hello world'
+    #  self.assertEqual(s.split(), ['hello', 'world'])
+    #  # check that s.split fails when the separator is not a string
+    #  with self.assertRaises(TypeError):
+    #      s.split(2)
+
+    #def test_algebra(self):
+    #  self.assertEqual('foo'.upper(), 'FOO')
 
 if __name__ == '__main__':
     unittest.main()
+    print("Done tests.py")
